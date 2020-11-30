68ba77bef88d79e2be446f5c2da8e914fa14a2c7
taman9333 <taman9333@gmail.com>
2020-11-30 21:55:16 +0200

    Refactor remaining types

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -21,6 +21,68 @@ class GildedRose
     end
   end
 
+  class AgedBrie
+    attr_reader :quality, :sell_in
+
+    def initialize(quality, sell_in)
+      @quality = quality
+      @sell_in = sell_in
+    end
+
+    def update
+      if quality < 50
+        @quality += 1
+      end
+      @sell_in -= 1
+      if sell_in < 0
+        if quality < 50
+          @quality += 1
+        end
+      end
+    end
+  end
+
+  class BackstagePass
+    attr_reader :quality, :sell_in
+
+    def initialize(quality, sell_in)
+      @quality = quality
+      @sell_in = sell_in
+    end
+
+    def update
+      if quality < 50
+        @quality += 1
+        if sell_in < 11
+          if quality < 50
+            @quality += 1
+          end
+        end
+        if sell_in < 6
+          if quality < 50
+            @quality += 1
+          end
+        end
+      end
+      @sell_in -= 1
+      if sell_in < 0
+        @quality = quality - quality
+      end
+    end
+  end
+
+  class Sulfuras
+    attr_reader :quality, :sell_in
+
+    def initialize(quality, sell_in)
+      @quality = quality
+      @sell_in = sell_in
+    end
+
+    def update
+    end
+  end
+
   def initialize(items)
     @items = items
   end
@@ -28,39 +90,25 @@ class GildedRose
   def update_quality
     @items.each do |item|
       if sulfuras?(item)
+        sulfuras = Sulfuras.new(item.quality, item.sell_in)
+        sulfuras.update
+        item.quality = sulfuras.quality
+        item.sell_in = sulfuras.sell_in
       elsif generic?(item)
         generic = Generic.new(item.quality, item.sell_in)
         generic.update
         item.quality = generic.quality
         item.sell_in = generic.sell_in
       elsif aged_brie?(item)
-        if item.quality < 50
-          item.quality = item.quality + 1
-        end
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        aged_brie = AgedBrie.new(item.quality, item.sell_in)
+        aged_brie.update
+        item.quality = aged_brie.quality
+        item.sell_in = aged_brie.sell_in
       elsif backstage_pass?(item)
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-        end
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0
-          item.quality = item.quality - item.quality
-        end
+        backstage_pass = BackstagePass.new(item.quality, item.sell_in)
+        backstage_pass.update
+        item.quality = backstage_pass.quality
+        item.sell_in = backstage_pass.sell_in
       end
     end
   end
