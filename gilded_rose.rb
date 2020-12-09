8dcc4058557f9445267397a9aa8a4bc4f0dde147
taman9333 <taman9333@gmail.com>
2020-12-09 21:05:27 +0200

    Add quality obj to backstage

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -18,6 +18,14 @@ module Inventory
         @amount += 1
       end
     end
+
+    def reset
+      @amount = 0
+    end
+
+    def less_than_50?
+      @amount < 50
+    end
   end
 
   class Generic
@@ -63,30 +71,30 @@ module Inventory
   end
 
   class BackstagePass
-    attr_reader :quality, :sell_in
+    attr_reader :sell_in
 
     def initialize(quality, sell_in)
-      @quality = quality
+      @quality = Quality.new(quality)
       @sell_in = sell_in
     end
 
+    def quality
+      @quality.amount
+    end
+
     def update
-      if quality < 50
-        @quality += 1
+      @quality.increase
+      if @quality.less_than_50?
         if sell_in < 11
-          if quality < 50
-            @quality += 1
-          end
+          @quality.increase
         end
         if sell_in < 6
-          if quality < 50
-            @quality += 1
-          end
+          @quality.increase
         end
       end
       @sell_in -= 1
       if sell_in < 0
-        @quality = quality - quality
+        @quality.reset
       end
     end
   end
