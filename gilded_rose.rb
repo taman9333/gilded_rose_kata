82d42936bfdaf147d1658cf2352b5ea6cbfb022a
taman9333 <taman9333@gmail.com>
2021-01-04 22:19:23 +0200

    Refactor generic type

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,4 +1,26 @@
 class GildedRose
+
+  class Generic
+    attr_reader :quality, :sell_in
+
+    def initialize(quality, sell_in)
+      @quality = quality
+      @sell_in = sell_in
+    end
+
+    def update
+      if quality > 0
+        @quality -= 1
+      end
+      @sell_in -= 1
+      if sell_in < 0
+        if quality > 0
+          @quality -= 1
+        end
+      end
+    end
+  end
+
   def initialize(items)
     @items = items
   end
@@ -7,28 +29,43 @@ class GildedRose
     @items.each do |item|
       if sulfuras?(item)
       elsif generic?(item)
-        handle_generic(item)
+        generic = Generic.new(item.quality, item.sell_in)
+        generic.update
+        item.quality = generic.quality
+        item.sell_in = generic.sell_in
       elsif aged_brie?(item)
-        handle_aged_brie(item)
+        if item.quality < 50
+          item.quality = item.quality + 1
+        end
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
       elsif backstage_pass?(item)
-        handle_backstage_pass(item)
+        if item.quality < 50
+          item.quality = item.quality + 1
+          if item.sell_in < 11
+            if item.quality < 50
+              item.quality = item.quality + 1
+            end
+          end
+          if item.sell_in < 6
+            if item.quality < 50
+              item.quality = item.quality + 1
+            end
+          end
+        end
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0
+          item.quality = item.quality - item.quality
+        end
       end
     end
   end
 
   private
-
-  def decrease_quality(item)
-    item.quality = item.quality - 1
-  end
-
-  def increase_quality(item)
-    item.quality = item.quality + 1
-  end
-
-  def quality_less_than_50(item)
-    item.quality < 50
-  end
 
   def aged_brie?(item)
     item.name == 'Aged Brie'
@@ -44,50 +81,6 @@ class GildedRose
 
   def generic?(item)
     !(sulfuras?(item) or backstage_pass?(item) or aged_brie?(item))
-  end
-
-  def handle_backstage_pass(item)
-    if quality_less_than_50(item)
-      increase_quality(item)
-      if item.sell_in < 11
-        if quality_less_than_50(item)
-          increase_quality(item)
-        end
-      end
-      if item.sell_in < 6
-        if quality_less_than_50(item)
-          increase_quality(item)
-        end
-      end
-    end
-    item.sell_in = item.sell_in - 1
-    if item.sell_in < 0
-      item.quality = item.quality - item.quality
-    end
-  end
-
-  def handle_generic(item)
-    if item.quality > 0
-      decrease_quality(item)
-    end
-    item.sell_in = item.sell_in - 1
-    if item.sell_in < 0
-      if item.quality > 0
-        decrease_quality(item)
-      end
-    end
-  end
-
-  def handle_aged_brie(item)
-    if quality_less_than_50(item)
-      increase_quality(item)
-    end
-    item.sell_in = item.sell_in - 1
-    if item.sell_in < 0
-      if quality_less_than_50(item)
-        increase_quality(item)
-      end
-    end
   end
 end
 
